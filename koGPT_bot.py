91d2dbcc1692cffea58a57c5ea2c678e266c9e0a
Sinwoo Park <112746031+KimPalZa@users.noreply.github.com>
2023-04-17 01:18:11 +0900

    fix: 작동되는 코드 작성
    AI와 구글의 도움을 받아 작동되는 코드를 작성.
    학습을 시키지 않아 이상한 소리만 한다.

--- koGPT_bot.py
+++ koGPT_bot.py
@@ -1,29 +1,31 @@
+import torch
+from transformers import GPT2LMHeadModel
+from transformers import PreTrainedTokenizerFast
 import discord
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from discord.ext import commands
 
-tokenizer = GPT2Tokenizer.from_pretrained("skt/kogpt2-base-v2")
-model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
+tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2", bos_token='</s>', eos_token='</s>', unk_token='<unk>', pad_token='<pad>', mask_token='<mask>') 
+model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+TOKEN = '*'
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.event
+async def on_ready():
+    print(f'Login bot: {bot.user}')
 
-    input_text = message.content
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+@bot.command()
+async def 입력(message,*,vars):
+    input_data = tokenizer.encode(vars)
+    print(vars)
+    gen = model.generate(torch.tensor([input_data]),max_length=128,
+                             repetition_penalty=2.0,
+                             pad_token_id=tokenizer.pad_token_id,
+                             eos_token_id=tokenizer.eos_token_id,
+                             bos_token_id=tokenizer.bos_token_id,
+                             use_cache=True)
+    generated = tokenizer.decode(gen[0,:].tolist())
+    print(generated)
+    await message.channel.send(generated)
 
-    sample_output = model.generate(
-        input_ids,
-        do_sample=True, 
-        max_length=100, 
-        top_k=50, 
-        top_p=0.95, 
-        num_return_sequences=1
-    )
-
-    response_text = tokenizer.decode(sample_output[0], skip_special_tokens=True)
-    await message.channel.send(response_text)
-
-client.run("YOUR_DISCORD_BOT_TOKEN")
+bot.run(TOKEN)
